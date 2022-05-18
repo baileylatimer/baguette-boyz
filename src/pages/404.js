16ecf9728a2a488cccfae481cb2d2dfcb7416e20
Bailey Latimer <latimerdesignco@gmail.com>
2022-05-18 14:27:02 -0700

    updated 404 title

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,26 @@ import SEO from '~/components/seo'
 import ScrollingText from "~/components/scrolling-text"
 import Snowflakes from "~/components/snowflakes"
 import Image from "~/components/pic"
+import Slashes from "~/components/elements/slashes"
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
     <div className="padding-top-xxxl container">
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div class="border radius-lg margin-bottom-md">
+        <div class="grid border-bottom">
+          <div class="flex justify-between col-4 items-center padding-sm">
+            <Slashes />
+            <h2 class="">404</h2>
+            <Slashes />
+          </div>
+          <div class="border-left border-right col-8 flex justify-center items-center padding-sm">
+            <h2 class="">Loading...</h2>
+          </div>
+  
+        </div>
+      </div>
+
       <Image filename="art/cover-art.jpg" />
       <ScrollingText />
       <Snowflakes />
