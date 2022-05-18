6afd6d786c0452f00c9755c0b3fbdc2798b389fc
Bailey Latimer <latimerdesignco@gmail.com>
2022-05-18 14:21:13 -0700

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 import SEO from '~/components/seo'
+import ScrollingText from "~/components/scrolling-text"
+import Snowflakes from "~/components/snowflakes"
+import Image from "~/components/pic"
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="padding-top-xxxl container">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Image filename="art/cover-art.jpg" />
+      <ScrollingText />
+      <Snowflakes />
+    </div>
   </>
 )
 
